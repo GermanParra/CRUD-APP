14a3a9d322a67636c72d1b2a51471cdeae130342
GermanParra <gerpar13@gmail.com>
2022-04-03 20:50:10 -0400

    Edit Action

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+from ast import Return
 from datetime import datetime
 import sqlite3
+from typing import Type
 from flask import Flask, render_template, request #, redirect, url_for, jsonify
 from models import DB, Albums, Tracks
 
@@ -25,13 +27,13 @@ def home():
     return render_template('home.html', Albums = albums, Rows_count=rows_count)
 
 
-@app.route('/album_insert', methods=['GET','POST'])
+@app.route('/album_insert', methods=['POST'])
 def album_insert():
-    row_info=[]
+    album_info=[]
     if request.method == 'POST':
         # Inserting New Row Info into Albums Table 
-        row_info = [request.form['title'], request.form['artist'], datetime.strptime(request.form['released'], '%Y-%m-%d')]
-        row = Albums(title=row_info[0], artist=row_info[1], released=row_info[2])
+        album_info = [request.form['title'], request.form['artist'], datetime.strptime(request.form['released'], '%Y-%m-%d')]
+        row = Albums(title=album_info[0], artist=album_info[1], released=album_info[2])
         DB.session.add(row)
         DB.session.commit()
 
@@ -41,10 +43,10 @@ def album_insert():
     rows_count = cursor.execute("SELECT COUNT(*) from Albums").fetchall()
     albums = cursor.execute("SELECT * from Albums").fetchall()
     
-    return render_template('insert.html', new_row_info=row_info, Albums = albums, Rows_count=rows_count)
+    return render_template('insert.html', new_row_info=album_info, Albums = albums, Rows_count=rows_count)
 
 
-@app.route('/reset', methods=['GET','POST'])
+@app.route('/reset', methods=['POST'])
 def reset():
     # Reseting Database
     DB.drop_all()
@@ -60,9 +62,38 @@ def reset():
     return render_template('reset.html', Albums = albums, Rows_count=rows_count)
 
 
-@app.route('/edit', methods=['GET','POST'])
+@app.route('/edit', methods=['POST', 'GET'])
 def edit():
-    return request.form["ROW_ID"]
+    conection = sqlite3.connect('database.db')
+    cursor = conection.cursor()
+
+    if request.method == 'POST':
+        album_info = request.form["row_info"][1:-2].split(',')
+        album_info[0] = int(album_info[0])
+        # Extracting Info From Albums Table to create HTML table
+        albums = cursor.execute("SELECT * from Albums").fetchall()
+        rows_count = cursor.execute("SELECT COUNT(*) from Albums").fetchall()
+
+        return render_template('album_edit.html', Rows_count=rows_count, Albums = albums, album_info = album_info)
+    else:
+        id = request.args['album_id']
+        title = request.args['edited_title']
+        artist = request.args['edited_artist']
+        released = request.args['edited_released']
+
+        row = Albums.query.get(id)
+        row.title = title
+        row.artist = artist
+        row.released = datetime.strptime(released, '%Y-%m-%d')
+
+        DB.session.commit()
+
+        albums = cursor.execute("SELECT * from Albums").fetchall()
+        rows_count = cursor.execute("SELECT COUNT(*) from Albums").fetchall()
+    
+        return render_template('home.html', Albums = albums, Rows_count=rows_count)
+        
+
 
 
 if __name__ == '__main__':

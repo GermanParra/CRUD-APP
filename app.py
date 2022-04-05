66331bf7e7e28ad68415dfaad68f1da59fd8a9ed
GermanParra <gerpar13@gmail.com>
2022-04-05 18:12:58 -0400

    All Actions Working

--- app.py
+++ app.py
@@ -50,8 +50,8 @@ def album_insert():
 
 
 @app.route('/edit_album', methods=['POST'])
-def edit():
-    if "row_id" in request.form:
+def edit_album():
+    if "album_id" in request.form:
         album_id = request.form["row_id"]
         album_info = Albums.query.filter_by(id=album_id).first()
         # Quering DB to display table and row count on HTML 
@@ -60,7 +60,7 @@ def edit():
         return render_template('album_edit.html', Rows_count=rows_count, Albums = albums, album_info = album_info)
     else:
         # Requesting Changes
-        id = request.form['album_id']
+        id = request.form['row_id']
         title = request.form['edited_title']
         artist = request.form['edited_artist']
         released = request.form['edited_released']
@@ -115,6 +115,51 @@ def song_insert():
     songs_count = Tracks.query.filter_by(album_id=album_id).count()
     songs = Tracks.query.filter_by(album_id=album_id) 
     return render_template('tracks.html', Songs=songs, songs_count=songs_count, album_info=album_info)
+
+
+@app.route('/edit_songs', methods=['POST'])
+def edit_songs():
+    if "album_id" in request.form:
+        song_id = request.form['song_id']
+        song_info = Tracks.query.filter_by(id=song_id).first()
+        album_info = Albums.query.filter_by(id=song_info.album_id).first()
+        # Quering DB to display table and row count on HTML 
+        songs_count = Tracks.query.filter_by(album_id=song_info.album_id).count()
+        songs = Tracks.query.filter_by(album_id=song_info.album_id) 
+        return render_template('tracks_edit.html', Songs=songs, songs_count=songs_count, song_info = song_info, album_info=album_info)
+    else:
+        # Requesting Changes
+        song_id = request.form['song_id']
+        name = request.form['name']
+        genre = request.form['genre']
+        duration = str(request.form['mins'])+':'+str(request.form['secs'])
+        # Replacing Changed features
+        song = Tracks.query.get(song_id)
+        song.name = name
+        song.genre = genre
+        song.duration = duration
+        # Saving Changes
+        DB.session.commit()
+        album_info = Albums.query.filter_by(id=song.album_id).first()
+        # Quering DB to display table and row count on HTML 
+        songs_count = Tracks.query.filter_by(album_id=song.album_id).count()
+        songs = Tracks.query.filter_by(album_id=song.album_id) 
+        return render_template('tracks.html', Songs=songs, songs_count=songs_count, album_info=album_info)
+
+
+@app.route('/song_delete', methods=['POST'])
+def song_delete():
+    song_id = request.form["song_id"]
+    song = Tracks.query.filter_by(id=song_id).first()
+    album_id = song.album_id
+    DB.session.delete(song)       
+    DB.session.commit()
+
+    album_info = Albums.query.filter_by(id=album_id).first()
+    # Quering DB to display table and row count on HTML 
+    songs_count = Tracks.query.filter_by(album_id=song.album_id).count()
+    songs = Tracks.query.filter_by(album_id=song.album_id) 
+    return render_template('tracks_delete.html', Songs=songs, songs_count=songs_count, album_info=album_info, song_info=song)
 
 
 if __name__ == '__main__':

fa5400b830a21efafa26007aa6cb143cce39995f
GermanParra <gerpar13@gmail.com>
2022-04-07 18:28:55 -0400

    Setting ENV variables

--- spotify.py
+++ spotify.py
@@ -1,12 +1,12 @@
 from os import getenv
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from SpotiKeys import CLIENT_ID, CLIENT_SECRET
 
 
-## Get API keys from .env
-#CLIENT_ID = getenv('CLIENT_ID')
-#CLIENT_SECRET = getenv('CLIENT_SECRET')
+
+# Get API keys from .env
+CLIENT_ID = getenv('CLIENT_ID')
+CLIENT_SECRET = getenv('CLIENT_SECRET')
 
 
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(CLIENT_ID, CLIENT_SECRET))
